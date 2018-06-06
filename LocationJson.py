875dd6250dc4cdcbd90ceaad30a387e069e56ad7
Supcomfreak <lunarmoon56@gmail.com>
2018-06-06 16:21:01 -0700

    Changed final format to dictionary

--- LocationJson.py
+++ LocationJson.py
@@ -2,11 +2,11 @@ import geopy.distance as gpd
 import json
 from datetime import datetime, timedelta, date
 import pytz
+from collections import defaultdict
 
+coordinate = (47.358913,-122.086783)
 
-coordinate = (47.651295,-122.0311216)
-
-locations = json.load(open('LocationHistory.json'))["locations"]
+locations = json.load(open('RoryJr.json'))["locations"]
 pointcount = len(locations)
 
 def dtFromMs(timestampMs):
@@ -17,7 +17,7 @@ locations.reverse()
 for location in locations:
 	if(location["accuracy"]<100):
 		tempcoordinate = (location["latitudeE7"]/10**7, location["longitudeE7"]/10**7)
-		if(gpd.distance(coordinate, tempcoordinate).m < 200):
+		if(gpd.distance(coordinate, tempcoordinate).m < 500):
 			times.append(location["timestampMs"])
 		else:
 			times.append("break")
@@ -28,31 +28,28 @@ b = "break"
 for time in times:
 	if(time != b):
 		if(b == "break"):
-			timeBlock[0] = time		
+			timeBlock[0] = time	
 		if(time == "break"):
 			timeBlock[1] = b
-			timeBlocks.append((timeBlock[0], timeBlock[1]))
+			timeBlocks.append([timeBlock[0], timeBlock[1]])
 	b = time
 
 for block in timeBlocks:
-	block = (dtFromMs(int(block[0])), dtFromMs(int(block[1])))
+	block[0] = dtFromMs(int(block[0]))
+	block[1] = dtFromMs(int(block[1]))
 
-start = []
-end = []
-a = datetime.min
-"""
-for block in timeBlocks:
-	if(len(timeBlocks) == 0):
-		break
-	
-	if(block[0].date() != a.date()):
-		if(len(start)>0):
-			end.append(b)
-		start.append(block[0])
-	b = block[1]
+sortedBlocks = defaultdict(list)
 
-workdays = zip(start, end) 
-"""
 for block in timeBlocks:
-	if(((int(block[1])-int(block[0]))/3600000)>0.01):
-		print(dtFromMs(int(block[0])), ", ", dtFromMs(int(block[1])), ", ", (int(block[1])-int(block[0]))/3600000)
+	sortedBlocks[block[0].date()].append((block[0], block[1]))
+
+final = []
+
+for key in sortedBlocks:
+	temp = sortedBlocks[key]
+	timeDelta = temp[-1][1]-temp[0][0]
+	timeAdditive = timedelta(microseconds = 0)
+	for block in sortedBlocks[key]:
+		timeAdditive += block[1]-block[0]
+	print(key, ",", timeDelta, ",", timeAdditive)
+
---
+++ LocationJson2.py
@@ -0,0 +1,7 @@
+import geopy.distance as gpd
+import json
+from datetime import datetime, dimedelta, date
+
+class timeline:
+	def __init__ (self, pathToFile):
+	
